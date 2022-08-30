6c8e9bc6109c042f46b38314bbb0112c17c78539
Amaru Zegarra <ctatutu@gmail.com>
2022-08-30 13:08:58 -0300

    para ver noma

---
+++ src/main/java/com/AmaruPortafolio/Amaruta/AmarutaApplication.java
@@ -0,0 +1,13 @@
+package com.AmaruPortafolio.Amaruta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmarutaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmarutaApplication.class, args);
+	}
+
+}
