a45433b46c19c292ae66b8c4eace38f292b4cb8a
Amaru Zegarra <ctatutu@gmail.com>
2022-10-24 20:50:27 -0300

    implements hys
    se implemento hys

---
+++ BackEnd/src/main/java/com/AmaruPortafolio/Amaruta/Repository/Rhys.java
@@ -0,0 +1,12 @@
+
+package com.AmaruPortafolio.Amaruta.Repository;
+
+import com.AmaruPortafolio.Amaruta.Entity.hys;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface Rhys extends JpaRepository<hys, Integer>{
+    Optional<hys> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+}
