bf2c71617dcd049a4b2d139dab87881a778467ce
Amaru Zegarra <ctatutu@gmail.com>
2022-10-26 16:43:46 -0300

    Update PersonaController.java

--- BackEnd/src/main/java/com/AmaruPortafolio/Amaruta/Controller/PersonaController.java
+++ BackEnd/src/main/java/com/AmaruPortafolio/Amaruta/Controller/PersonaController.java
@@ -68,7 +68,7 @@ public class PersonaController {
         persona.setNombre(dtopersona.getNombre());
         persona.setApellido(dtopersona.getApellido());
         persona.setDescripcion(dtopersona.getDescripcion());
-        persona.setImg(dtopersona.getImg());
+        persona.setImg(dtopersona.getImg()); 
         
         personaService.save(persona);
         return new ResponseEntity(new Mensaje("Persona actualizada"), HttpStatus.OK);
